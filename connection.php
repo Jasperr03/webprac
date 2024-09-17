61562e29b9ffea7a21cf76a159eb13f1768c923a
Jasperr03 <jasperellevera03@gmail.com>
2024-09-17 10:50:06 +0800

    updated  connections.php

--- connection.php
+++ connection.php
@@ -6,8 +6,8 @@ $password = "";
 $dbname = "db_webprac";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8mb4", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $webprac = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $webprac->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
