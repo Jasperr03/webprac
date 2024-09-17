33d384638f574f114ed5b96113b616a29b0cff18
Jasperr03 <jasperellevera03@gmail.com>
2024-09-17 08:43:37 +0800

    added index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WebPractice</title>
+</head>
+<body>
+    <div>
+        <?php echo 'hello World'?>
+    </div>
+</body>
+</html>
