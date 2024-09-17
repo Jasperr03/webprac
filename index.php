736cb569f5d3ae6cb0d4684861ebc1fc3e7905e9
Jasperr03 <jasperellevera03@gmail.com>
2024-09-17 11:33:26 +0800

    added nav bar

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once 'connection.php';
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,12 +12,27 @@ require_once 'connection.php';
     <link href="assets/img/webpracLogo.png" rel="icon">
     <link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
-<body>
-    <div class="alert alert-danger">
-        <?php echo 'hello World'; ?>
-    </div>
+
+<body class="sb-nav-fixed">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary px-2">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="assets/img/webpracLogo.png" alt="webpracLogo" style="height: 0.3in;">
+                WebPrac
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-expanded="true">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="navbar-collapse collapse hide" id="">
+                <div class="navbar-collapse flex-grow-1 text-right" style="padding-left: 20px">
+                    
+                </div>
+            </div>
+        </div>
+    </nav>
 
     <script src="js/jquery-3.7.1.min.js"></script>
     <script src="js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
