519ebc5907068b6bed7e7d7a4a277b34aa0e64f5
Jasperr03 <jasperellevera03@gmail.com>
2024-09-17 09:49:22 +0800

    added bootstrap5

--- index.php
+++ index.php
@@ -4,10 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WebPractice</title>
+    <link href="assets/img/webpracLogo.png" rel="icon">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <div>
-        <?php echo 'hello World'?>
+    <div class="alert alert-danger">
+        <?php echo 'hello World'; ?>
     </div>
+
+    <script src="js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
